b9e8c5ed7f092a6058101e6709d27b89274aa865
cwebsterz <cwebsterzornes@live.com>
2017-06-28 09:35:56 -0400

    step one completed

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,44 +1,62 @@
 import React from 'react'
 import Header from '../containers/header'
 import {
-  Button,
-  List,
-  ImageListItem,
-  SimpleListItem
+	Button,
+	List,
+	ImageListItem,
+	SimpleListItem
 } from 'jrs-react-components'
 import LinkButton from '../components/link-button'
+import { map } from 'ramda'
+import { connect } from 'react-redux'
 
-const Home = function() {
-  return (
-    <div>
-      <Header />
-      <main>
-        <div className="mw6 center mt2 tc">
-          <List>
-            <SimpleListItem
-              title="Add New Favorite"
-              link={<LinkButton to="/new">Add</LinkButton>}
-            />
-            <ImageListItem
-              id={1}
-              title="What about Bob?"
-              image="https://images-na.ssl-images-amazon.com/images/M/MV5BMTQxMjU2ODk4N15BMl5BanBnXkFtZTgwODQzNTcxMTE@._V1_SX300.jpg"
-              link={<LinkButton to="/show/1">Details</LinkButton>}
-            />
-          </List>
-        </div>
-      </main>
-    </div>
-  )
+const Home = function(props) {
+	function li(fave) {
+		return (
+			<ImageListItem
+				id={fave.id}
+				key={fave.id}
+				title={fave.title}
+				image={fave.poster}
+				year={fave.year}
+				link={<LinkButton to={`/show/${fave.id}`}>Details</LinkButton>}
+			/>
+		)
+	}
+
+	return (
+		<div>
+			<Header />
+			<main>
+				<div className="mw6 center mt2 tc">
+					<List>
+						<SimpleListItem
+							title="Add New Favorite"
+							link={<LinkButton to="/new">Add</LinkButton>}
+						/>
+						{map(li, props.favorites)}
+					</List>
+				</div>
+			</main>
+		</div>
+	)
 }
 
-export default Home
+const connector = connect(mapStateToProps)
+
+function mapStateToProps(state) {
+	return {
+		favorites: state.favorites
+	}
+}
+
+export default connector(Home)
 
 function openDocs(e) {
-  if (/localhost/.test(window.location.href)) {
-    window.location = 'http://localhost:5000'
-  } else {
-    window.location =
-      'https://github.com/jrs-innovation-center/jrscode-react-starter#jrs-react-starter-kit'
-  }
+	if (/localhost/.test(window.location.href)) {
+		window.location = 'http://localhost:5000'
+	} else {
+		window.location =
+			'https://github.com/jrs-innovation-center/jrscode-react-starter#jrs-react-starter-kit'
+	}
 }
